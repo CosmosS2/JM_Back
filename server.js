b16affc35a3ad3ed89d30a26125741c09c02ce6c
Luis Cespedes <lxluxo23@gmail.com>
2025-02-09 14:32:08 -0300

    PR

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const { connectToDatabase } = require('./config/dbConfig');
-
 const userRoutes = require('./routes/user');
 const clientRoutes = require('./routes/cliente');
-const authRoutes = require('./routes/auth'); 
-const productRoutes = require('./routes/product'); 
-const ticketRoutes = require('./routes/boleta'); 
-
-const PORT = 5000;
+const authRoutes = require('./routes/auth');
+const productRoutes = require('./routes/product');
+const ticketRoutes = require('./routes/boleta');
 
 require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
@@ -25,9 +23,9 @@ app.use(morgan('combined'));
 
 app.use('/user', userRoutes);
 app.use('/client', clientRoutes);
-app.use('/auth', authRoutes); 
-app.use('/product', productRoutes); 
-app.use('/ticket', ticketRoutes); 
+app.use('/auth', authRoutes);
+app.use('/product', productRoutes);
+app.use('/ticket', ticketRoutes);
 
 connectToDatabase().then(() => {
     app.listen(PORT, () => {
